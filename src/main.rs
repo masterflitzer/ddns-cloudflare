b523e70e408dbc02e634327e67d2476392e2d294
Masterflitzer <60170948+Masterflitzer@users.noreply.github.com>
2023-01-10 04:42:09 +0100

    basic deps, structs, config, api call, ip

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,32 @@
-fn main() {
-    println!("Hello, world!");
+mod structs;
+
+use std;
+// use std::net::{IpAddr, Ipv4Addr, Ipv6Addr};
+use reqwest::Client;
+use std::net::Ipv4Addr;
+
+#[tokio::main]
+async fn main() {
+    let config: structs::Config = confy::load("ddns-cloudflare", None).unwrap();
+    println!("{:#?}", config);
+    println!(
+        "{}",
+        confy::get_configuration_file_path("ddns-cloudflare", None)
+            .unwrap()
+            .display()
+    );
+
+    let http: Client = reqwest::Client::new();
+    let ipv4: Ipv4Addr = get_ipv4(http).await.unwrap();
+    println!("ip: {}", ipv4);
+}
+
+async fn get_ipv4(http: Client) -> Result<Ipv4Addr, reqwest::Error> {
+    let response = http
+        .get("https://api.ipify.org?format=json")
+        .send()
+        .await?
+        .json::<structs::Ipify>()
+        .await?;
+    Ok(response.ip)
 }
