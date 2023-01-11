25a06fb98c45d842dc0040e60ed64f956f0f6993
Masterflitzer <60170948+Masterflitzer@users.noreply.github.com>
2023-01-11 21:46:43 +0100

    update config parsing

--- src/main.rs
+++ src/main.rs
@@ -1,32 +1,32 @@
+mod config;
 mod structs;
 
-use std;
-// use std::net::{IpAddr, Ipv4Addr, Ipv6Addr};
-use reqwest::Client;
-use std::net::Ipv4Addr;
+use reqwest::Client as HttpClient;
+use std::{error::Error, net::Ipv4Addr};
 
 #[tokio::main]
-async fn main() {
-    let config: structs::Config = confy::load("ddns-cloudflare", None).unwrap();
-    println!("{:#?}", config);
+async fn main() -> Result<(), Box<dyn Error>> {
+    let config = config::get_config().expect("An error occurred while parsing the configuration. Please consult the readme for an example configuration.");
+    println!("api token: \t\"{}\"", config.api_token);
+    println!("1st zone name: \t\"{}\"", config.zones[0].name);
     println!(
-        "{}",
-        confy::get_configuration_file_path("ddns-cloudflare", None)
-            .unwrap()
-            .display()
+        "1st zone - 1st record: \t\"{}\"",
+        config.zones[0].records[0]
     );
 
-    let http: Client = reqwest::Client::new();
-    let ipv4: Ipv4Addr = get_ipv4(http).await.unwrap();
+    let ipv4: Ipv4Addr = get_ipv4().await?;
     println!("ip: {}", ipv4);
+    Ok(())
 }
 
-async fn get_ipv4(http: Client) -> Result<Ipv4Addr, reqwest::Error> {
+async fn get_ipv4() -> Result<Ipv4Addr, reqwest::Error> {
+    let http: HttpClient = HttpClient::new();
     let response = http
         .get("https://api.ipify.org?format=json")
         .send()
         .await?
         .json::<structs::Ipify>()
         .await?;
+
     Ok(response.ip)
 }
