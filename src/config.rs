70296dac3585e4f7ebdf14fe1ede81371ae3379e
Masterflitzer <60170948+Masterflitzer@users.noreply.github.com>
2023-01-12 12:08:09 +0100

    add clap & update code

--- src/config.rs
+++ src/config.rs
@@ -3,24 +3,31 @@ use directories::ProjectDirs;
 use std::{
     fs::File,
     io::{Error, ErrorKind, Read},
-    path::PathBuf,
+    path::{Path, PathBuf},
 };
 
-fn get_name() -> String {
+fn cargo_name() -> String {
     env!("CARGO_PKG_NAME").replace('_', "-")
 }
 
-pub fn get_config() -> Result<Config, Error> {
-    let name = get_name();
+pub fn path() -> Result<PathBuf, Error> {
+    let name = cargo_name();
 
     let project_dirs =
         ProjectDirs::from("", "", &name).ok_or_else(|| Error::from(ErrorKind::NotFound))?;
     let config_dir = ProjectDirs::config_dir(&project_dirs);
 
-    std::fs::create_dir_all(config_dir)?;
-
     let mut path = PathBuf::from(config_dir);
     path.push(format!("{}.toml", name));
+    Ok(path)
+}
+
+pub fn get(path: impl AsRef<Path>) -> Result<Config, Error> {
+    std::fs::create_dir_all(
+        path.as_ref()
+            .parent()
+            .ok_or_else(|| Error::from(ErrorKind::NotFound))?,
+    )?;
 
     let mut file = File::options()
         .read(true)
--- src/main.rs
+++ src/main.rs
@@ -1,32 +1,76 @@
 mod config;
 mod structs;
 
+use clap::Parser;
 use reqwest::Client as HttpClient;
-use std::{error::Error, net::Ipv4Addr};
+use std::{net::Ipv4Addr, process::exit};
+use structs::{Args, Ipify};
 
 #[tokio::main]
-async fn main() -> Result<(), Box<dyn Error>> {
-    let config = config::get_config().expect("An error occurred while parsing the configuration. Please consult the readme for an example configuration.");
-    println!("api token: \t\"{}\"", config.api_token);
-    println!("1st zone name: \t\"{}\"", config.zones[0].name);
-    println!(
-        "1st zone - 1st record: \t\"{}\"",
-        config.zones[0].records[0]
-    );
+async fn main() {
+    let args = Args::parse();
 
-    let ipv4: Ipv4Addr = get_ipv4().await?;
+    let config_path = match args.config {
+        Some(x) => x,
+        None => match config::path() {
+            Ok(x) => x,
+            Err(e) => {
+                println!("An unexpected error occurred while trying to get the path of the configuration file.\n\n{}", e);
+                exit(1)
+            }
+        },
+    };
+
+    if args.configuration {
+        println!("{}", config_path.display());
+        return;
+    }
+
+    let config = match config::get(config_path) {
+        Ok(x) => x,
+        Err(_) => {
+            println!("An error occurred while parsing the configuration.\nPlease consult the readme for an example configuration.");
+            exit(1)
+        }
+    };
+    println!("api token: \t\"{}\"", config.api_token);
+    println!("zone name: \t\"{}\"", config.zones[0].name);
+    println!("zone record: \t\"{}\"", config.zones[0].records[1]);
+
+    let http: HttpClient = HttpClient::new();
+
+    let ipv4: Ipv4Addr = determine_ipv4(http).await.unwrap();
     println!("ip: {}", ipv4);
-    Ok(())
 }
 
-async fn get_ipv4() -> Result<Ipv4Addr, reqwest::Error> {
-    let http: HttpClient = HttpClient::new();
+async fn obtain_zone_id(http: HttpClient, zone_name: String) -> Result<String, reqwest::Error> {
+    todo!()
+}
+
+async fn obtain_record_id(
+    http: HttpClient,
+    zone_id: String,
+    zone_name: String,
+    record_name: String,
+) -> Result<String, reqwest::Error> {
+    todo!()
+}
+
+async fn update_ip(http: HttpClient) {
+    todo!()
+}
+
+async fn determine_ipv4(http: HttpClient) -> Result<Ipv4Addr, reqwest::Error> {
     let response = http
         .get("https://api.ipify.org?format=json")
         .send()
         .await?
-        .json::<structs::Ipify>()
+        .json::<Ipify>()
         .await?;
 
     Ok(response.ip)
+}
+
+async fn determine_ipv6() {
+    todo!()
 }
