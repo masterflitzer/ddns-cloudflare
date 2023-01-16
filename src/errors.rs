2184ff486e9062933f1e2b2317eb9bf439205b42
Masterflitzer <60170948+Masterflitzer@users.noreply.github.com>
2023-01-16 22:23:40 +0100

    fix deserializing (enums are bad in serde)

--- src/errors.rs
+++ src/errors.rs
@@ -1,4 +1,4 @@
-use std::{error::Error, io::Error as IOError, io::ErrorKind as IOErrorKind};
+use std::{error::Error, io::Error as IOError};
 
 #[derive(Debug)]
 pub(crate) enum ErrorKind {
@@ -8,8 +8,8 @@ pub(crate) enum ErrorKind {
     IPv6,
     JsonDeserialize,
     JsonSerialize,
-    NoSuccessHttp { name: String, type_: String },
-    NoSuccessJson { name: String, type_: String },
+    NoSuccessHttp(String),
+    NoSuccessJson(String),
     API,
     Unknown(Box<dyn Error>),
 }
@@ -17,24 +17,20 @@ pub(crate) enum ErrorKind {
 pub(crate) fn handle_errors(kind: &ErrorKind) {
     match kind {
         ErrorKind::API => println!("The HTTP client encountered an unexpected error while trying to connect to the API"),
-        ErrorKind::Config(e) => println!("An error occurred while parsing the configuration. Please consult the readme for an example configuration.\n\n{}", e),
-        ErrorKind::ConfigPath(e) => println!("An error occurred while trying to get the path to the configuration file.\n\n{}", e),
+        ErrorKind::Config(e) => println!("An error occurred while parsing the configuration. Please consult the readme for an example configuration.\n{}", e),
+        ErrorKind::ConfigPath(e) => println!("An error occurred while trying to get the path to the configuration file.\n{}", e),
         ErrorKind::IPv4 => println!("An error occurred while trying to determine the IPv4 address"),
         ErrorKind::IPv6 => println!("An error occurred while trying to determine the IPv6 address"),
         ErrorKind::JsonDeserialize => println!("An error occurred while deserializing JSON"),
         ErrorKind::JsonSerialize => println!("An error occurred while serializing JSON"),
-        ErrorKind::NoSuccessHttp { name, type_ } => println!(
-            "{}: Skipping {} because HTTP status code was not between 200-299",
-            name, type_
+        ErrorKind::NoSuccessHttp(name) => println!(
+            "{}: Skipping record/zone because HTTP status code was not between 200-299",
+            name
         ),
-        ErrorKind::NoSuccessJson { name, type_ } => println!(
-            "{}: Skipping {} because JSON payload did not contain {{ \"success\": true }}",
-            name, type_
+        ErrorKind::NoSuccessJson(name) => println!(
+            "{}: Skipping record/zone because JSON payload did not contain {{ \"success\": true }}",
+            name
         ),
-        ErrorKind::Unknown(e) => println!("An unexpected error occured!\n\n{}", e),
+        ErrorKind::Unknown(e) => println!("An unexpected error occured!\n{}", e),
     };
-}
-
-pub(crate) fn create_generic_error() -> IOError {
-    IOError::from(IOErrorKind::Other)
 }
