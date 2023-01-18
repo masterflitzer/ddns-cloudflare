e8491cc0227840f7756961a86776effc1e349b5d
Masterflitzer <60170948+Masterflitzer@users.noreply.github.com>
2023-01-18 17:51:09 +0100

    update naming

--- src/errors.rs
+++ src/errors.rs
@@ -2,7 +2,7 @@ use std::{error::Error, io::Error as IOError};
 
 #[derive(Debug)]
 pub(crate) enum ErrorKind {
-    API,
+    Api,
     Config(IOError),
     ConfigPath(IOError),
     IPv4,
@@ -16,7 +16,7 @@ pub(crate) enum ErrorKind {
 
 pub(crate) fn handle_errors(kind: &ErrorKind) {
     match kind {
-        ErrorKind::API => println!("The HTTP client encountered an unexpected error while trying to connect to the API"),
+        ErrorKind::Api => println!("The HTTP client encountered an unexpected error while trying to connect to the API"),
         ErrorKind::Config(e) => println!("An error occurred while parsing the configuration. Please consult the readme for an example configuration.\n{}", e),
         ErrorKind::ConfigPath(e) => println!("An error occurred while trying to get the path to the configuration file.\n{}", e),
         ErrorKind::IPv4 => println!("An error occurred while trying to determine the IPv4 address"),
