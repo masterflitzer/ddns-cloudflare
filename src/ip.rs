f89666b7f1bac3ec992379ac65d7714bca1e512e
masterflitzer <git@sl.datavitec.com>
2025-02-02 18:41:41 +0100

    update ip.rs

--- src/ip.rs
+++ src/ip.rs
@@ -9,16 +9,19 @@ use std::{
     time::Duration,
 };
 
+const IPV4_UNSPECIFIED: IpAddr = IpAddr::V4(Ipv4Addr::UNSPECIFIED);
+const IPV6_UNSPECIFIED: IpAddr = IpAddr::V6(Ipv6Addr::UNSPECIFIED);
+
 pub(crate) async fn determine_ip(config: &Config) -> (Option<Ipv4Addr>, Option<Ipv6Addr>) {
-    let ipv4 = determine_ipv4().await;
+    let ipv4 = determine_ipv4(config).await;
     let ipv6 = determine_ipv6(config).await;
     (ipv4, ipv6)
 }
 
 pub(crate) async fn query_ip(ip_enum: IpAddr) -> Option<IpAddr> {
     let local_ip: IpAddr = match ip_enum {
-        IpAddr::V4(_) => IpAddr::V4(Ipv4Addr::UNSPECIFIED),
-        IpAddr::V6(_) => IpAddr::V6(Ipv6Addr::UNSPECIFIED),
+        IpAddr::V4(_) => IPV4_UNSPECIFIED,
+        IpAddr::V6(_) => IPV6_UNSPECIFIED,
     };
 
     let http = HttpClient::builder()
@@ -28,7 +31,7 @@ pub(crate) async fn query_ip(ip_enum: IpAddr) -> Option<IpAddr> {
         .ok()?;
 
     let response = http
-        .get("https://one.one.one.one/cdn-cgi/trace")
+        .get("https://cloudflare.com/cdn-cgi/trace")
         .send()
         .await
         .ok()?
@@ -45,7 +48,7 @@ pub(crate) async fn query_ip(ip_enum: IpAddr) -> Option<IpAddr> {
         .collect();
 
     let ip = data.get("ip")?;
-    let ip_address = IpAddr::from_str(ip).ok()?;
+    let ip_address = IpAddr::from_str(ip).ok()?.to_canonical();
     Some(ip_address)
 }
 
@@ -69,10 +72,10 @@ fn eui48_to_modified_eui64(eui48: &[u8; 6]) -> Option<[u8; 8]> {
     Some(modified_eui64)
 }
 
-pub(crate) async fn determine_ipv4() -> Option<Ipv4Addr> {
-    let ipv4_unspecified = IpAddr::V4(Ipv4Addr::UNSPECIFIED);
+pub(crate) async fn determine_ipv4(config: &Config) -> Option<Ipv4Addr> {
+    let _ = config;
 
-    let ip = query_ip(ipv4_unspecified).await?;
+    let ip = query_ip(IPV4_UNSPECIFIED).await?;
 
     let ipv4 = match ip {
         IpAddr::V4(x) => Some(x),
@@ -83,20 +86,23 @@ pub(crate) async fn determine_ipv4() -> Option<Ipv4Addr> {
 }
 
 pub(crate) async fn determine_ipv6(config: &Config) -> Option<Ipv6Addr> {
-    let ipv6_unspecified = IpAddr::V6(Ipv6Addr::UNSPECIFIED);
-
-    let ip = query_ip(ipv6_unspecified).await?;
+    let ip = query_ip(IPV6_UNSPECIFIED).await?;
 
     let ipv6 = match ip {
         IpAddr::V4(_) => None,
         IpAddr::V6(x) => Some(x),
     }?;
 
+    if config.ipv6.prefer_outgoing {
+        return Some(ipv6);
+    }
+
     let (prefix, _) = split_ipv6(&ipv6)?;
 
     let network_interfaces = list_afinet_netifas().ok()?;
     let ipv6_addresses = network_interfaces
-        .into_iter()
+        .iter()
+        .cloned()
         .filter_map(|(_, ip)| match ip.to_canonical() {
             IpAddr::V4(_) => None,
             IpAddr::V6(x) => Some(x),
@@ -108,29 +114,34 @@ pub(crate) async fn determine_ipv6(config: &Config) -> Option<Ipv6Addr> {
         })
         .collect::<Vec<_>>();
 
-    if ipv6_addresses.len() <= 1 {
+    if ipv6_addresses.is_empty() {
+        return None;
+    }
+
+    if ipv6_addresses.len() == 1 {
         return ipv6_addresses.first().cloned();
     }
 
     if config.ipv6.prefer_eui64 {
         let mac = get_mac_address().ok()?;
         let suffix = eui48_to_modified_eui64(&mac?.bytes())?;
-        if let Some(x) = ipv6_addresses.iter().find(|ip| match split_ipv6(ip) {
-            Some((_, s)) => s == suffix,
-            None => false,
-        }) {
-            return Some(x.to_owned());
+        if let Some(ipv6_eui64) = ipv6_addresses
+            .iter()
+            .cloned()
+            .find(|ip| match split_ipv6(ip) {
+                Some((_, s)) => s == suffix,
+                None => false,
+            })
+        {
+            return Some(ipv6_eui64.to_owned());
         };
     }
 
-    if config.ipv6.prefer_outgoing {
-        return Some(ipv6);
-    }
-
-    return ipv6_addresses
-        .into_iter()
+    ipv6_addresses
+        .iter()
+        .cloned()
         .filter(|ip| ip != &ipv6)
         .collect::<Vec<_>>()
         .first()
-        .cloned();
+        .cloned()
 }
